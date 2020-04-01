eba749b69ff7d20b5ee7524a0dffaf168c4c73d6
Your Name <diego-vespa2020@outlook.com.br>
2020-04-01 11:33:32 -0300

    finalização da segunda tela - formulário

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-const button = document
-  .querySelector("button.fat")
-  .addEventListener("click", function () {
-    document
-      .querySelector("#footer")
-      .classList
-      .toggle("hide")
-    
-  })
+function onOff() {
+  document.querySelector("#modal").classList.toggle("hide")
+  
+}
+
+
 
