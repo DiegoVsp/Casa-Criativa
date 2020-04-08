951bfbed7d15a99aa05c9f4798e55a66a04e2423
Your Name <diego-vespa2020@outlook.com.br>
2020-04-08 16:03:33 -0300

    projeto WorkShop finalizado

--- server.js
+++ server.js
@@ -2,41 +2,13 @@
 const express = require("express");
 const server = express();
 
-const ideas = [
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-    title: "Cursos de Programação",
-    category: "Estudo",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea labore temporibus ex magnam earum dolore",    
-    url: "https://rocketseat.com.br"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-    title: "Exercicios",
-    category: "Saúde",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea labore temporibus ex magnam earum dolore",    
-    url: "https://rocketseat.com.br"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-    title: "Meditação",
-    category: "Mentalidade",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea labore temporibus ex magnam earum dolore",    
-    url: "https://rocketseat.com.br"
-  },
-  {
-    img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-    title: "Karaokê",
-    category: "Diversão em Família",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea labore temporibus ex magnam earum dolore",    
-    url: "https://rocketseat.com.br"
-  },
-]
-
-
+const db = require("./db");
 
 // configurar arquivos estáticos(css, scripts, imagens)
 server.use(express.static("public"))
+
+// Habilitar uso do req.body
+server.use(express.urlencoded ({ extended: true}))
 
 // configuração nunjucks
 const nunjucks = require("nunjucks")
@@ -50,19 +22,79 @@ nunjucks.configure("views",{
 // e capturo o pedido do cliente para responder
 server.get("/", function(req, res){
 
-  const reversedIdeas = [...ideas].reverse()  //não é uma referencia de ideas, agora é uma variável nova
-  let lastIdeas = []
-  for(let idea of reversedIdeas) { 
+  db.all(`SELECT * FROM ideas`, function(err, rows){
+    if(err) {
+      console.log(err)
+      return res.send("Erro no Banco de Dados")
+    }
+     
+    const reversedIdeas = [...rows].reverse()  //não é uma referencia de ideas, agora é uma variável nova
+    let lastIdeas = []
+    for(let idea of reversedIdeas) { 
     if(lastIdeas.length < 2){
       lastIdeas.push(idea)
     }
   }
   
   return res.render("index.html", {ideas: lastIdeas})
+  })
+
+  
 })
 server.get("/ideias", function(req,res){
-  const reversedIdeas = [...ideas].reverse()
-  return res.render("ideias.html", {ideas: reversedIdeas})
+  
+
+  db.all(`SELECT * FROM ideas`, function(err, rows){
+    if(err) {
+      console.log(err)
+      return res.send("Erro no Banco de Dados")
+    }
+    const reversedIdeas = [...rows].reverse()
+
+    return res.render("ideias.html", {ideas: reversedIdeas})  
+  })
+
+  
 })
+server.post("/", function(req, res){ 
+
+   // Inserir dados na tabela
+  const query =`
+    INSERT INTO ideas(
+    image,
+    title,
+    category,
+    description,
+    link
+  ) 
+    VALUES (?,?,?,?,?);
+  `
+  const values = [
+    req.body.image,
+    req.body.title,
+    req.body.category,
+    req.body.description,
+    req.body.link
+ ]     
+    db.run(query, values, function(err){
+      if(err) {
+        console.log(err)
+        return res.send("Erro no Banco de Dados")
+      }
+      return res.redirect("/ideias")
+    })
+
+  
+})
+// server.delete("/ideas", function(req, res){
+//   // Deletar dados na tabela
+//   db.run(`DELETE FROM ideas WHERE id = ?`, [id], function(err){
+//     if(err) {
+//       console.log(err)
+//       return res.send("Erro no Banco de Dados")
+//     }
+//     return res.redirect("/ideias")
+//   })
+// })
 // liguei meu servidor na porta 3000
 server.listen(3000)
